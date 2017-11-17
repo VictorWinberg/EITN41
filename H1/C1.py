b9416acb777994932e24cda7cfeba6ad4621567f
VictorWinberg <dat14vwi@student.lu.se>
2017-11-17 12:54:45 +0100

    add H1 C1 cut-and-choose

--- H1/C1.py
+++ H1/C1.py
@@ -1,6 +1,8 @@
 import binascii
 import hashlib
 from sys import argv
+from random import randrange, sample
+from functools import reduce
 
 def get_n(p, q):
     return p * q
@@ -9,7 +11,7 @@ def totient(p, q):
     return (p - 1) * (q - 1)
 
 def modinv_x(x, a, n):
-    x = toInt(x)
+    x = toInt(x) if type(x) != int else x
     return hex(modinv(a, n) * x % n)
 
 # (a * x) == 1 (mod n) => x = modinv(a) (mod n)
@@ -29,7 +31,7 @@ def euc_algorithm(a, n):
     return a, x0, y0
 
 # Encryption, decryption and blind signatures
-def encrypt(m, n, e):
+def encrypt(m, e, n):
     m_int = byte_to_int(m.encode('utf-8'))
     if m_int != m_int % n:
         raise Exception('Max length of message exceeded')
@@ -38,25 +40,34 @@ def encrypt(m, n, e):
     c = int_to_byte(c_int).decode('latin-1')
     return c
 
-def decrypt(c, n, e, d):
+def decrypt(c, e, n, d):
     c_int = byte_to_int(c.encode('latin-1'))
     m_int = pow(c_int, d, n)
     return int_to_byte(m_int).decode('utf-8')
 
 def hash(x):
+    x = hex(x) if type(x) == int else x
     return hashlib.sha1(x.encode('utf-8')).hexdigest()
 
-def blind_hash(x, r, e):
+def blind_hash(x, r, e, n):
     hash_x = toInt(hash(x))
-    return hex(pow(r, e) * hash_x)
+    return hex(pow(r, e) * hash_x % n)
+
+def blind(x, r, e, n):
+    return hex(pow(r, e) * x % n)
 
 def sign(x, n, d):
-    x = toInt(x)
+    x = toInt(x) if type(x) != int else x
     return hex(pow(x, d, n))
 
-def verify(x, signature, n, e):
+def verify(x, signature, e, n):
     x, signature = toInt(x), toInt(signature)
     return x == pow(signature, e, n)
+
+def blind_signature(m, r, e, n, d):
+    h_blind = blind_hash(m, r, e, n)
+    h_blind_signed = sign(h_blind, n, d)
+    return modinv_x(h_blind_signed, r, n)
 
 # Converters
 def byte_to_int(x, byteorder='big'):
@@ -85,20 +96,52 @@ if __name__ == "__main__":
         p, q, e = map(int, argv[1:])
     else:
         p, q = 671998030559713968361666935769, 282174488599599500573849980909
-        e = 5
+        e = 17
 
     n = get_n(p, q)
-    d = modinv(e, totient(p, q))
-    print('e:', e, '\nn:', n, '\nd:', d)
+    priv_key = modinv(e, totient(p, q))
+    print('e:', e, '\nn:', n, '\nd:', priv_key)
 
-    m, r = 'Hi give me 10 coins, plz!', 123
-    h = hash(m)
-    h_blind = blind_hash(m, r, e)
+    print('\nsign:', blind_signature('Give 10 coins!', 7, e, n, priv_key), '\n')
 
-    h_blind_signed = sign(h_blind, n, d)
+    k, ID, XY, quads, B, B_client = 8, 1234, {}, {}, [], []
 
-    h_signed = modinv_x(h_blind_signed, r, n)
-    print('h:', h, '\nsign:', h_signed, '\nverified:', verify(h, h_signed, n, e))
+    for i in range(2 * k):
+        a, c, d, r = [ randrange(100, 1000) for i in range(4) ]
+        x, y = toInt(hash(a + c)), toInt(hash(a ^ ID + d))
+        B_i = blind(x * y, r, e, n)
+        B.append(B_i)
+        quads[B_i] = a, c, d, r
+        XY[B_i] = x, y
+
+    B_verify = sample(B, len(B) // 2)
+    B_sign = [ B_i for B_i in B if B_i not in B_verify ]
+
+    B_verified = []
+
+    for B_i in B_verify:
+        a, c, d, r = quads[B_i]
+        x, y = toInt(hash(a + c)), toInt(hash(a ^ ID + d))
+        B_verified.append(B_i == blind(x * y, r, e, n))
+
+    print('verified:', all(B_i is True for B_i in B_verified))
+
+    B_signed = []
+
+    for B_i in B_sign:
+        B_signed.append(int(sign(B_i, n, priv_key), 16))
+
+    B_blind_signature = hex(reduce(lambda x, y: x * y % n, B_signed))
+
+    R_arr = []
+    for B_i in B_sign:
+        R_arr.append(quads[B_i][3])
+
+    R = reduce(lambda x, y: x * y % n, R_arr)
+    print('R:', R)
+
+    B_signature = modinv_x(B_blind_signature, R, n)
+    print('S:', B_signature)
 
     # Debugging
     import pdb
