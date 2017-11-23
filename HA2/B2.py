bd1cac9daab1a7d47d89b0594b3145dcc5edc35e
VictorWinberg <dat14vwi@student.lu.se>
2017-11-23 18:20:38 +0100

    modify and refactor of HA2 B2

--- HA2/B2.py
+++ HA2/B2.py
@@ -2,62 +2,55 @@ from functools import reduce
 from pcapfile import savefile
 from itertools import chain
 
-def get_ip_src(pkt):
-    return pkt.packet.payload.src.decode('UTF8')
-
-def get_ip_dst(pkt):
-    return pkt.packet.payload.dst.decode('UTF8')
-
 def ipadress_to_int(ipadress):
     return reduce(lambda x, y: int(x) << 8 | int(y), ipadress.split('.'))
 
-testcap = open(input(), 'rb')
-capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
+def read_packets(filepath):
+    testcap = open(filepath, 'rb')
+    capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
 
-target_adress, mix_adress, m = input(), input(), int(input())
+    packets = list((
+        pkt.packet.payload.src.decode('UTF8'),
+        pkt.packet.payload.dst.decode('UTF8')
+        ) for pkt in capfile.packets)
 
-packets = [ {
-    'src': get_ip_src(pkt),
-    'dst': get_ip_dst(pkt)}
-    for pkt in capfile.packets
-]
+    return list(zip(*packets))
 
-batches = []
-prevMix = 1
+def learning_phase(packets, target_adress, mix_adress, m):
+    sources, destinations = packets
+    R, R_all, target = [], [], 0
 
-for packet in packets:
-    isMix = int(packet['src'] == mix_adress)
-    if(prevMix != isMix): batches.append([])
+    while True:
+        target = sources.index(target_adress, target)
+        mix_start = sources.index(mix_adress, target)
+        try:
+            mix_end = destinations.index(mix_adress, mix_start)
+        except ValueError:
+            break
+        r = set(destinations[mix_start:mix_end])
+        R_all.append(r)
+        if all(r.isdisjoint(ri) for ri in R):
+            R.append(r)
+        target = mix_end
 
-    p = [ packet['src'], packet['dst'] ]
-    batches[-1].append(p[isMix])
+    R_other = [ r for r in R_all if r not in R ]
+    return R, R_other
 
-    prevMix = isMix
-    # print ('{}\t{}'.format(packet['src'], packet['dst']))
+def excluding_phase(R, R_rest):
+    for r in R_rest:
+        indices = [ i for i, ri in enumerate(R) if r & ri != set() ]
+        if len(indices) == 1:
+            index = indices[0]
+            R[index] &= r
 
-batches = list(zip(batches[::2], batches[1::2]))
-R_all = []
+    R = list(set().union(*R))
+    return R
 
-for batch in batches:
-    src, dst = batch
-    if target_adress in src:
-        R_all.append(dst)
+if __name__ == "__main__":
+    filepath, target_adress, mix_adress, m = input(), input(), input(), int(input())
+    packets = read_packets(filepath)
+    R, R_rest = learning_phase(packets, target_adress, mix_adress, m)
+    targets = excluding_phase(R, R_rest)
 
-R_disjoint = [ set(R_all[0]) ]
-for R_i in R_all:
-    if all(set(R_i).isdisjoint(R_j) for R_j in R_disjoint):
-        R_disjoint.append(set(R_i))
-
-R_rest = [ set(R) for R in R_all if set(R) not in R_disjoint ]
-
-for R in R_rest:
-    for i, R_i in enumerate(R_disjoint):
-        R_i_disjoint = R & R_i == set()
-        R_j_disjoint = [R & R_j == set() for j, R_j in enumerate(R_disjoint) if i != j]
-        if not R_i_disjoint and all(R_j_disjoint):
-            R_disjoint[i] &= set(R)
-
-R_disjoint = list(set().union(*R_disjoint))
-print('m:', m, ', found:', len(R_disjoint), 'of', len(R_all))
-
-print(sum(map(ipadress_to_int, R_disjoint)))
+    print('m:', m, 'found:', len(targets))
+    print(sum(map(ipadress_to_int, targets)))
