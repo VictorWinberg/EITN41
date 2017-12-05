4d13a752e91045e6a18b71d1fdaac19f40958eae
VictorWinberg <dat14vwi@student.lu.se>
2017-12-05 18:06:33 +0100

    add instruction for HA4 B3 encode and decode

--- HA4/B3.py
+++ HA4/B3.py
@@ -44,6 +44,78 @@ def MGF1(mgfSeed, maskLen, Hash=hashlib.sha1, hLen=20):
 
   return hexlify(T[:maskLen]).decode('utf-8')
 
+def OAEP_encode(M, seed, Hash=hashlib.sha1):
+  """ EME-OAEP encoding
+  Input:
+    M - the message to be encoded
+    seed
+  Output:
+    EM - the encoded message
+  """
+  # a.  If the label L is not provided, let L be the empty string.
+  #     Let lHash = Hash(L), an octet string of length hLen (see
+  #     the note below).
+  #
+  # b.  Generate a padding string PS consisting of k - mLen -
+  #     2hLen - 2 zero octets.  The length of PS may be zero.
+  #     c.  Concatenate lHash, PS, a single octet with hexadecimal
+  #     value 0x01, and the message M to form a data block DB of
+  #     length k - hLen - 1 octets as
+  # 
+  #        DB = lHash || PS || 0x01 || M.
+  # 
+  # d.  Generate a random octet string seed of length hLen.
+  # 
+  # e.  Let dbMask = MGF(seed, k - hLen - 1).
+  # 
+  # f.  Let maskedDB = DB \xor dbMask.
+  # 
+  # g.  Let seedMask = MGF(maskedDB, hLen).
+  # 
+  # h.  Let maskedSeed = seed \xor seedMask.
+  # 
+  # i.  Concatenate a single octet with hexadecimal value 0x00,
+  #     maskedSeed, and maskedDB to form an encoded message EM of
+  #     length k octets as
+  # 
+  #        EM = 0x00 || maskedSeed || maskedDB.
+
+def OAEP_decode(EM):
+  """ EME-OAEP decoding
+  Input:
+    EM - the encoded message to be decoded
+  Output:
+    M - the decoded message
+  """
+  # a.  If the label L is not provided, let L be the empty string.
+  #     Let lHash = Hash(L), an octet string of length hLen (see
+  #     the note in Section 7.1.1).
+  # 
+  # b.  Separate the encoded message EM into a single octet Y, an
+  #     octet string maskedSeed of length hLen, and an octet
+  #     string maskedDB of length k - hLen - 1 as
+  # 
+  #        EM = Y || maskedSeed || maskedDB.
+  # 
+  # c.  Let seedMask = MGF(maskedDB, hLen).
+  # 
+  # d.  Let seed = maskedSeed \xor seedMask.
+  # 
+  # e.  Let dbMask = MGF(seed, k - hLen - 1).
+  # 
+  # f.  Let DB = maskedDB \xor dbMask.
+  # 
+  # g.  Separate DB into an octet string lHash' of length hLen, a
+  #     (possibly empty) padding string PS consisting of octets
+  #     with hexadecimal value 0x00, and a message M as
+  # 
+  #        DB = lHash' || PS || 0x01 || M.
+  # 
+  #     If there is no octet with hexadecimal value 0x01 to
+  #     separate PS from M, if lHash does not equal lHash', or if
+  #     Y is nonzero, output "decryption error" and stop.  (See
+  #     the note below.)
+
 mgfSeed = '0123456789abcdef'
 maskLen = 30
 
