de2c116ffd3eb3cabcdc8f1cc60dcc77af5a46b0
VictorWinberg <dat14vwi@student.lu.se>
2017-12-12 16:49:34 +0100

    add HA5 C1 without refactoring

---
+++ HA5/C1.py
@@ -0,0 +1,68 @@
+from random import randrange
+from hashlib import sha1
+
+def modinv(a, n):
+  g, x, _ = euc_algorithm(a, n)
+  if g == 1:
+    return x % n
+
+def euc_algorithm(a, n):
+  x0, x1, y0, y1 = 1, 0, 0, 1
+  while n != 0:
+    q, a, n = a // n, n, a % n
+    x0, x1 = x1, x0 - q * x1
+    y0, y1 = y1, y0 - q * y1
+  return a, x0, y0
+
+def hex2(x):
+  return '{}{:x}'.format('0' * (len(hex(x)) % 2), x)
+
+def read_buffer():
+  lines = []
+  while True:
+    try:
+      lines.append(input())
+    except EOFError:
+      break
+  return lines
+
+# from https://github.com/eit-lth/Advanced-Web-Security_EITN41/blob/master/5%20Data%20Representation%20and%20PKI/code/jacobi.py
+def jacobi(a, m):
+  j = 1
+  a %= m
+  while a:
+  	t = 0
+  	while not a & 1:
+  		a = a >> 1
+  		t += 1
+  	if t & 1 and m % 8 in (3, 5):
+  		j = -j
+  	if (a % 4 == m % 4 == 3):
+  		j = -j
+  	a, m = m % a, a
+  return j if m == 1 else 0
+
+def rec_hash(identity, n):
+  H = sha1(identity)
+  a_byte, a_hex = H.digest(), H.hexdigest()
+  residue = jacobi(int(a_hex, 16), n)
+  return a_hex if residue == 1 else rec_hash(a_byte, n)
+
+
+k_bry = b'walterwhite@crypto.sec'
+p = 0x9240633d434a8b71a013b5b00513323f
+q = 0xf870cfcd47e6d5a0598fc1eb7e999d1b
+
+n = p * q
+a = int(rec_hash(k_bry, n), 16)
+
+r = pow(a, (n + 5 - (p + q)) // 8, n)
+
+print('r', hex2(r))
+
+lines = read_buffer()
+
+decrypt = [ jacobi(int(s, 16) + 2*r, n) for s in lines ]
+
+decode = int(''.join(map(lambda x: '1' if x == 1 else '0', decrypt)), 2)
+print('msg:', decode)
